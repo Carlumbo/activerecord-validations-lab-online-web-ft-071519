89fa2b7b954ded35caf11567ed1a3deb20f9a2c1
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-10-03 20:41:42 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   validates :content, length: {min: 250 }
   validates :summary, length: {max: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  
+
 end
