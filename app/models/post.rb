2a824e6b7e57d270b0c1df2dba95cc1b9e1e6643
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-10-03 20:45:47 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,16 +2,16 @@ class Post < ActiveRecord::Base
   validates :content, length: {min: 250 }
   validates :summary, length: {max: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  validate :is_clickbait? 
+  validate :is_clickbait?
 
   CLICK_BAIT_PATTERNS = [ /Won't Believe/i,/Secret/i,/Top [0-9]*/i, /Guess/i]
 
-  def is_clickbait? 
-    if CLICK_BAIT_PATTERNS.NONE? do |p| 
+  def is_clickbait?
+    if CLICK_BAIT_PATTERNS.NONE? do |p|
       p.match title
-    end 
+    end
     errors.add(:title, "must be clickbait")
-    end 
-  end 
-  
+    end
+  end
+
 end
